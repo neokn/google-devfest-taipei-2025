1b1f8f4224fd9a0f7db89d9609d8e26e579cd670
Neo Kusanagi <jr@neo.kn>
2025-11-30 10:24:31 +0800

    feat: add playwright mcp

--- src/genkit.ts
+++ src/genkit.ts
@@ -1,9 +1,20 @@
 import { googleAI } from '@genkit-ai/google-genai';
-import { genkit, z } from 'genkit';
+import { genkit, z, type DynamicResourceAction, type ToolAction } from 'genkit';
+import { createMcpHost } from '@genkit-ai/mcp';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 const ai = genkit({
     plugins: [googleAI()],
 });
+
+async function getToolsAndResources(): Promise<{ tools: Array<ToolAction>, resources: Array<DynamicResourceAction> }> {
+    const host = createMcpHost(JSON.parse(await fs.readFile(path.join(process.cwd(), 'config/mcp-config.json'), 'utf-8')));
+    const tools = await host.getActiveTools(ai);
+    const resources = await host.getActiveResources(ai);
+
+    return { tools, resources };
+}
 
 export const petFlow = ai.defineFlow(
     {
@@ -41,6 +52,8 @@ export const discordFlow = ai.defineFlow(
             botUserId: input.botUserId,
             prompt: input.prompt,
         }, {
+            ... await getToolsAndResources(),
+            toolChoice: 'auto',
             messages: input.messages
         });
         return (await response).text;
