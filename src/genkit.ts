1b5e4f0569ed01b5c6a8b416a6b602e169f2eeff
Neo Kusanagi <jr@neo.kn>
2025-11-30 13:13:39 +0800

    fix: prompt name

--- src/genkit.ts
+++ src/genkit.ts
@@ -48,7 +48,7 @@ export const petFlow = ai.defineFlow(
     },
     async ({ prompt }) => {
 
-        const { response } = ai.prompt('pet').stream({
+        const { response } = ai.prompt('cat').stream({
             prompt: prompt,
         });
 
